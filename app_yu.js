35aed8f65163a83265a00c50ee0ac23a8db02fdd
yujin-5 <yonggari_gari@naver.com>
2024-11-20 17:53:46 +0900

    add menu btn

--- app_yu.js
+++ app_yu.js
@@ -1,29 +1,9 @@
+const $dropdownBtn = document.querySelector('.dropdown-toggle');
+const $dropdownMenu = document.querySelector('.dropdown-menu');
 
-// const $days = document.querySelector('.days')
+//클릭 시 드롭다운 메뉴 열고 닫는 이벤트
+$dropdownBtn.addEventListener('click', function () {
+    $dropdownMenu.classList.toggle('show');
+});
 
 
-// $days.forEach(day => {
-//     const $newDiv = document.createElement('div');
-//     $newDiv.classList.add('day');
-//     $newDiv.textContent = day[i+1];
-// });
-
-const year = 2024;
-const month = 11;
-const daysInMonth = new Date(year, month, 0).getDate();
-const startDay = new Date(year, month-1 , 1).getDay();
-
-const $days = document.querySelector('.days');
-
-for (let i = 0; i< startDay; i++){
-    const $emptyDiv = document.createElement('div');
-    $emptyDiv.classList.add('day', 'empty'); 
-    $days.appendChild($emptyDiv);
-}
-
-for (let day = 1; day <= daysInMonth; day++){
-    const $newDiv = document.createElement('div');
-    $newDiv.classList.add('day');
-    $newDiv.textContent = day;
-    $days.appendChild($newDiv);
-}
