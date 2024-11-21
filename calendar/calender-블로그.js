2093fdb9d751557391e7c05f42e51e54264a84b6
mabbakk <siyun145928@gmail.com>
2024-11-21 17:49:23 +0900

    11-21-mabbakk

---
+++ calendar/calender-블로그.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+// ========== toggle =========== //
+
+ // 토글 상태 확인 (선택 사항)
+ document.getElementById('toggle').addEventListener('change', (event) => {
+  if (event.target.checked) {
+    console.log('Toggle is ON');
+  } else {
+    console.log('Toggle is OFF');
+  }
+});
+
+// ========== toggle =========== //
